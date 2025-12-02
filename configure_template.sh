c4b3e09f230c8af787939a2e08dfa7c7229f4b8d
Santiago Iregui <santiago.iregui@kuleuven.be>
2025-12-02 14:15:59 +0000

    Fixed configuration script

--- configure_template.sh
+++ configure_template.sh
@@ -1,64 +1,84 @@
 #!/bin/bash
 
+#  Copyright (c) 2025 KU Leuven, Belgium
+#
+#  Author: Santiago Iregui
+#  email: <santiago.iregui@kuleuven.be>
+#
+#  GNU Lesser General Public License Usage
+#  Alternatively, this file may be used under the terms of the GNU Lesser
+#  General Public License version 3 as published by the Free Software
+#  Foundation and appearing in the file LICENSE.LGPLv3 included in the
+#  packaging of this file. Please review the following information to
+#  ensure the GNU Lesser General Public License version 3 requirements
+#  will be met: https://www.gnu.org/licenses/lgpl.html.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+
 #
 # Script for changing properly the name of the template in an easy way.
 #
-# Author: Santiago Iregui (santiago.iregui@kuleuven.be)
 
-export RED='\e[38;5;198m'
-export DEFAULTC='\e[39m'
+bold=$(tput bold)
+normal=$(tput sgr0)
+RED='\e[38;5;198m'
+DEFAULTC='\e[39m'
 CWD=$(pwd)
 
-echo -e "\v \e[31m${bold}Warning: $DEFAULTC ${normal}"
+echo -e "\v ${RED}${bold}Warning:${DEFAULTC}${normal}"
 read -p "You are about to change the name of the package. It is recommended that you delete the local git repository first (rm -rf <package_directory>/.git). Are you sure you want to proceed? (y/n) " -n 1 -r
 echo " "
 
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   echo -e 'The name of the package was NOT changed'
-  return
+  exit 0
 fi
 
 echo -e "\vPlease provide the location of the package. Press ENTER if OK, otherwise edit: "
-# retrieve the directory in which this script is located:
 package_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# ask for user input + suggest retrieved directory as default value:
 read -e -i "$package_directory" -p "" BASE
-
-# if no input, then take the default value:
 BASE="${BASE:-$package_directory}"
 package_name=${BASE##*/}
 
-echo -e "\v \e[31m${bold}Warning: $DEFAULTC ${normal} Please make sure that the following is the name of the package:"
-echo -e "\v \t $package_name \v"
+echo -e "\v ${RED}${bold}Warning:${DEFAULTC}${normal} Please make sure that the following is the name of the package:"
+echo -e "\v\t $package_name \v"
 echo -e "If not, please abort and provide the proper package directory in the previous step.\v"
 
 read -p "Please provide the new name of the package or type q to quit: " NEW_NAME
 
-if [ "x$NEW_NAME" = "x" ]; then
-  echo -e 'You need to provide a non empty name. Operation canceled'
-  return
+if [ -z "$NEW_NAME" ]; then
+  echo -e 'You need to provide a non-empty name. Operation canceled.'
+  exit 1
 fi
 if [ "$NEW_NAME" = "q" ]; then
-  echo -e 'You have canceled the operation. The name of the package was NOT changed'
-  return
+  echo -e 'You have canceled the operation. The name of the package was NOT changed.'
+  exit 0
 fi
 
 echo "The new name is: $NEW_NAME"
-cd $package_directory
-# This changes the name recursively in every file contained in the package:
-egrep -lRZ "$package_name" . | xargs -0 -l sed -i -e "s/$package_name/$NEW_NAME/g"
+cd "$package_directory" || exit 1
 
-# Now change the name of the package folder:
+# Recursively replace occurrences, excluding .git and build dirs
+grep -lRZ "$package_name" . --exclude-dir=.git --exclude-dir=build --exclude-dir=install | \
+xargs -0 -l sed -i -e "s|$package_name|$NEW_NAME|g"
+
+mv "include/$package_name" "include/$NEW_NAME"
+
+# Rename the package folder
 cd ..
-mv $package_name $NEW_NAME
+mv "$package_name" "$NEW_NAME"
 
-#Returns to the initial directory of the terminal
+# Return to the original directory
 if [ "${CWD##*/}" == "$package_name" ]; then
-  cd $NEW_NAME
+  cd "$NEW_NAME" || exit 1
 else
-  cd $CWD
+  cd "$CWD" || exit 1
 fi
 
-echo -e "\v \e[32m The package was configured correctly.$DEFAULTC"
-echo -e "It is recommended that now you initialize a new local git repository by running:"
+
+echo -e "\v \e[32mThe package was configured correctly.${DEFAULTC}"
+echo -e "It is recommended that you now initialize a new local git repository by running:"
 echo -e "\v\tgit init"
